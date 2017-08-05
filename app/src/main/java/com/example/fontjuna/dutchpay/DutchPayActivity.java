11dd5560516998c2881fccec904c63dadb82913a
fontjuna <fontjuna@gmail.com>
2017-08-05 17:31:54 +0900

    레이아웃 수정

--- app/src/main/java/com/example/fontjuna/dutchpay/DutchPayActivity.java
+++ app/src/main/java/com/example/fontjuna/dutchpay/DutchPayActivity.java
@@ -25,12 +25,26 @@ public class DutchPayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dutch_pay);
 
-        EditText editText = (EditText) findViewById(R.id.goods_edit_text);
-        editText.setHint(
-                "금액" + DELIMITER_MONEY + "이름" + DELIMITER_RATIO + "배율"
-                        + DELIMITER_MEMBER + "..." + DELIMITER_ITEM + "..."
-                        + " ('" + DELIMITER_RATIO + "배율' 생략하면 '" + DELIMITER_RATIO + "1'임)"
-        );
+        String information = "구분자로 [ : , ! / ] 괄호안의 4개 문자를 사용합니다";
+        information += "\n : 걷을금액과 낼사람들을 구분합니다";
+        information += "\n , 낼사람들 서로간을  구분 합니다";
+        information += "\n ! 그사람의 배율 (A는 2인분이면 A!2로 입력)";
+        information += "\n / 걷을 금액이 또 있으면 / 다음에 이어서 입력";
+        information += "\n상황1)5000원을 A,B,C 이렇게 세사람이 똑 같이 나눈다면";
+        information += "\n입력1)5000:A,B,C";
+        information += "\n상황2)상황1 에서 B는 다른 사람보다 1.5배 내야 한다면";
+        information += "\n입력2)5000:A,B!1.5,C (!를 사용 B!2 로 입력)";
+        information += "\n상황3)상황2 에 더해서 3000원을 A와 C가 똑같이 더 낸다면";
+        information += "\n입력3)5000:A,B!2,C/3000:A,C";
+
+        TextView resultTextView = (TextView) findViewById(R.id.result_text_view);
+        resultTextView.setHint(information);
+//        EditText editText = (EditText) findViewById(R.id.goods_edit_text);
+//        editText.setHint(
+//                "금액" + DELIMITER_MONEY + "이름" + DELIMITER_RATIO + "배율"
+//                        + DELIMITER_MEMBER + "..." + DELIMITER_ITEM + "..."
+//                        + " ('" + DELIMITER_RATIO + "배율' 생략하면 '" + DELIMITER_RATIO + "1'임)"
+//        );
     }
 
     public void onClick_calc_button(View view) {
@@ -49,17 +63,19 @@ public class DutchPayActivity extends AppCompatActivity {
         if (pos < 1) {
             pos = 3; // 기본 단위 1,000원5000
         }
-        int unit = new int[]{100, 500, 1000, 5000}[pos - 1];
+        int unit = new int[]{1, 5, 10, 50, 100, 500, 1000}[pos - 1];
 
         int money = 0;
+        String temp = "";
         String text = "총금액 = " + mAmount;
         for (String key : resultNames.keySet()) {
             money = (int) ((resultNames.get(key) + unit - 1) / unit) * unit;
-            text += "\n" + key + " : " + money;
+            temp += "\n" + key + " : " + money;
             mRemain += money;
         }
         text += "\n걷는금액 : " + mRemain;
         text += "\n남는금액 : " + (mRemain - mAmount);
+        text += temp;
 
         result.setText(text);
         mAmount = 0;
